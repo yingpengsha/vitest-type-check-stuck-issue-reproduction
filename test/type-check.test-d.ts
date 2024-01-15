d1b5bbb01fe1fe7b246b58951829f46dfc376fa9
yingpengsha <yingpengsha@kuaishou.com>
2024-01-15 15:20:24 +0800

    fix: update test cases

--- test/type-check.test-d.ts
+++ test/type-check.test-d.ts
@@ -1,8 +1,24 @@
-import { describe, expectTypeOf, test } from 'vitest';
-import { squared } from '../src/basic.js';
+import { describe, expectTypeOf, test } from "vitest";
+import { squared } from "../src/basic.js";
 
-describe('type testing', () => {
-  test('basic return type', () => {
-    expectTypeOf(squared(1)).toEqualTypeOf<number>();
+describe("type testing", () => {
+  describe("type testing", () => {
+    test("basic return type", () => {
+      expectTypeOf(squared(1)).toEqualTypeOf<number>();
+    });
+
+    // Commenting this describe will not block the test
+    describe("type testing", () => {
+      test("basic return type", () => {
+        expectTypeOf(squared(1)).toEqualTypeOf<number>();
+      });
+    });
+  });
+
+  // Or choosing to comment the describe won't block the test
+  describe("type testing", () => {
+    test("basic return type", () => {
+      expectTypeOf(squared(1)).toEqualTypeOf<number>();
+    });
   });
 });
